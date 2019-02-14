4fed29d86f97028649e13de1e560acdb6daa0dab
VINICIUSG INTERN/LGESP RViniciusg.internD MC SW(viniciusg.intern@lge.com) <viniciusg.intern@lge.com>
2019-02-14 09:40:58 -0200

    first commit

---
+++ app/src/main/java/com/example/viniciusgintern/popularmovies/DetailActivity.java
@@ -0,0 +1,25 @@
+package com.example.viniciusgintern.popularmovies;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+public class DetailActivity extends AppCompatActivity {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+        this.mViewHolder.detailImage = findViewById(R.id.detailImage);
+        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(this.mViewHolder.detailImage);
+    }
+
+    private static class ViewHolder{
+        ImageView detailImage;
+    }
+
+}
---
+++ app/src/main/java/com/example/viniciusgintern/popularmovies/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.viniciusgintern.popularmovies;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ViewHolder mViewHolder = new ViewHolder();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        this.mViewHolder.buttonNext = findViewById(R.id.buttonNext);
+        this.mViewHolder.buttonNext.setOnClickListener(this);
+
+        // Find the toolbar view inside the activity layout
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        // Sets the Toolbar to act as the ActionBar for this Activity window.
+        // Make sure the toolbar exists in the activity and is not null
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public void onClick(View v){
+        int id = v.getId();
+        if(id==R.id.buttonNext){
+            Intent intent = new Intent(this, DetailActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private static class ViewHolder{
+        Button buttonNext;
+    }
+
+}
